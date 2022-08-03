58c4e9c6e53bb7a44049a856d67bc47034f9a54d
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-08-03 18:20:59 -0300

    Requisito 2 concluído

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
+import Loading from './pages/Loading';
 import NotFound from './pages/NotFound';
 
 class App extends React.Component {
@@ -21,6 +22,7 @@ class App extends React.Component {
           <Route exact path="/favorites" component={ Favorites } />
           <Route exact path="/profile" component={ Profile } />
           <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="/loading" component={ Loading } />
           <Route component={ NotFound } />
         </Switch>
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,65 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
-  render() {
-    return (
-      <div data-testid="page-login" />
-    );
-  }
+    state = {
+      isDisabled: true,
+      userName: '',
+    }
+
+    handleValidationButton = (event) => {
+      this.setState({ userName: event.target.value }, () => {
+        const { userName } = this.state;
+        const minLength = 3;
+        const validateForName = userName.length < minLength;
+        this.setState({ isDisabled: validateForName });
+      });
+    }
+
+    saveUserName = async () => {
+      const { userName } = this.state;
+      const { history } = this.props;
+      history.push('/loading');
+      await createUser({ name: userName });
+      history.push('/search');
+    }
+
+    render() {
+      const { isDisabled, userName } = this.state;
+
+      return (
+        <div data-testid="page-login">
+
+          <form>
+
+            <label htmlFor="login-name-input">
+              <input
+                type="text"
+                data-testid="login-name-input"
+                value={ userName }
+                onChange={ this.handleValidationButton }
+              />
+            </label>
+
+            <label htmlFor="login-submit-button">
+              <input
+                type="button"
+                data-testid="login-submit-button"
+                disabled={ isDisabled }
+                onClick={ this.saveUserName }
+                value="Entrar"
+              />
+            </label>
+
+          </form>
+        </div>
+      );
+    }
 }
+
+Login.propTypes = {
+  history: PropTypes.string.isRequired,
+};
 
 export default Login;
