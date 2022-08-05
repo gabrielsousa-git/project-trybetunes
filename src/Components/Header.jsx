8081316b850d64df68f461ce6fd23b6d4fbb5792
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-08-04 23:09:53 -0300

    Requisito 4 concluído

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
+// import Search from './pages/Search';
+// import Favorites from './pages/Favorites';
+// import Profile from './pages/Profile';
 
 class Header extends Component {
   state = {
@@ -26,7 +30,12 @@ class Header extends Component {
           loading ? (
             <Loading />
           ) : (
-            <p data-testid="header-user-name">{ name }</p>
+            <div>
+              <p data-testid="header-user-name">{ name }</p>
+              <Link data-testid="link-to-search" to="/search">Search</Link>
+              <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+              <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+            </div>
           )
         }
       </header>
