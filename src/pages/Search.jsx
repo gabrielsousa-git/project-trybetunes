a3f2f5af015308dbc5290c8ed1af096e571c1389
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-08-06 07:03:59 -0300

    Requisito 5 concluído

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,53 @@ import React, { Component } from 'react';
 import Header from '../Components/Header';
 
 class Search extends Component {
+  state = {
+    isDisabled: true,
+    bandName: '',
+  }
+
+  handleValidationButton = (event) => {
+    this.setState({ bandName: event.target.value }, () => {
+      const { bandName } = this.state;
+      const minLength = 2;
+      const validateForName = bandName.length < minLength;
+      this.setState({ isDisabled: validateForName });
+    });
+  }
+
   render() {
+    const { isDisabled, bandName } = this.state;
+
     return (
-      <div data-testid="page-search">
-        <Header />
-        <h1>Search</h1>
-      </div>
+      <section>
+
+        <header data-testid="page-search">
+          <Header />
+          <h1>Search</h1>
+        </header>
+
+        <main>
+          <form action="">
+            <label htmlFor="search-artist-input">
+              <input
+                type="text"
+                data-testid="search-artist-input"
+                value={ bandName }
+                onChange={ this.handleValidationButton }
+              />
+            </label>
+            <label htmlFor="search-artist-button">
+              <input
+                type="button"
+                data-testid="search-artist-button"
+                disabled={ isDisabled }
+                value="Procurar"
+              />
+            </label>
+          </form>
+        </main>
+
+      </section>
     );
   }
 }
